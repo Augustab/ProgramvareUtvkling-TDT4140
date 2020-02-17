538e38be3508a4131c5549286f11ab7d0a55efec
August Asheim Birkeland <augustab@stud.ntnu.no>
2020-02-17 17:00:01 +0100

    laster opp register-app

--- django/settings.py
+++ django/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.register.apps.RegisterConfig',
 ]
 
 MIDDLEWARE = [
@@ -50,7 +51,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'PUdjango.urls'
-CRISPY_TEMPLATE_PACK= 'bootstrap4'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
