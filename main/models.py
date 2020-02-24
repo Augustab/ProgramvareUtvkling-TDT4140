84c09c19ec63ec33186fcb6ab17f949da7b1eb98
JHailemariam <jonathsh@stud.ntnu.no>
2020-02-24 15:58:33 +0100

    Lagde Room model, la til tabell i se_rom.html + mindre endringer

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Room(models.Model):
+    room_choices = [('S', 'Single Occupancy'), ('D', 'Double Occupancy'),('B', 'Both Single and Double Occupancy')]
+
+    room_no = models.CharField(max_length=5) #primary key
+    available = models.BooleanField(default=False)
+    capacity = models.IntegerField(default=None)
+    room_type = models.CharField(choices=room_choices, max_length=1, default=None)
+
--- untitled1/settings.py
+++ untitled1/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'signup.apps.SignupConfig',
-    'crispy_forms'
+    'crispy_forms',
+    'main',
 ]
 
 MIDDLEWARE = [
