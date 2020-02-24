cf2fcbe88fa698a37645a72cc8ec01c69bb48ce1
Mads Eliassen <madsbel@stud.ntnu.no>
2020-02-24 16:54:44 +0100

    fiksa viwes

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, reverse
 from django.http import HttpResponse, HttpResponsePermanentRedirect
+from .models import Room
 
 
 # Create your views here.
@@ -14,7 +15,10 @@ def home(response):
 
 ##denne må vi ha, den tegner se_rom.
 def se_rom(response):
-    return render(response, "../templates/se_rom.html")
+    list_of_rooms = Room.objects.all()
+    context = {'list_of_rooms': list_of_rooms}
+    return render(response, "../templates/se_rom.html", context)
+
 
 ##Denne funksjonen sørger for at dersom du ikke har skrevet noe i url-en (dvs = "http://127.0.0.1:8000/") så skal du redirectes til http://127.0.0.1:8000/home/ dette fordi vi vil at brukerene skal være på hjem siden når man starter programmet.
 def redirect(request):
