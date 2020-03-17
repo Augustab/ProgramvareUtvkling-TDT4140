8d7c0e1ff532b298b28d85767ee1790d3d610f86
August Asheim Birkeland <augbir@gmail.com>
2020-03-17 18:49:10 +0100

    Endringer for å prøve å få heroku til å fungere.

--- untitled1/settings.py
+++ untitled1/settings.py
@@ -77,6 +77,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -155,7 +156,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 ######## kodelinjene under (staticfiles..) gjør at django klarer å finne static-mappen vår. OM vi ikke har med den vil den ikke finne staticfilene.
 
