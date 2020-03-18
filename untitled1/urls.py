8b3800e60d0353033220b7c5517cbef8ce8f6696
August Asheim Birkeland <augbir@gmail.com>
2020-03-18 19:17:29 +0100

    admin side styling heroku

--- untitled1/urls.py
+++ untitled1/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 import signup.views as v
 import main.views as v2
@@ -30,5 +32,4 @@ urlpatterns = [
     path("vaskehjelp/", v2.vaskehjelp, name="vaskehjelp"),
     path('', v2.redirect, name="homeredirectfraingenting"),
     path('', include("django.contrib.auth.urls")),
-]
-urlpatterns += staticfiles_urlpatterns()
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
