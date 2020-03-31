f8973fd2aa5df1926bcc59767bea15ec2630f5c6
Jenny Heggen Thiis <jennyht@stud.ntnu.no>
2020-03-31 10:35:37 +0200

    Update tests.py

--- main/tests.py
+++ main/tests.py
@@ -1,11 +1,10 @@
-'''Her finner du våre automatiske tester'''
 from django.test import TestCase, Client
-from main.models import Room
-from django.urls import reverse
+from main.models import Room, Booking
+from django.contrib.auth.models import User
 
 
 
-#tester models
+#tester Room-model
 class RoomModelTest(TestCase):
     @classmethod
     def setUpTestData(cls):  # setter opp et testobjekt
@@ -64,7 +63,41 @@ class RoomModelTest(TestCase):
         max_length = room._meta.get_field('room_type').max_length
         self.assertEquals(max_length, 1)
 
+# tester Booking-Model
+class BookingModelTest(TestCase):
+    # oppretter testobjekt
+    @classmethod
+    def setUpTestData(cls):
+        test_user = User.objects.create(username='testuser2')
+        test_user.set_password('123password')
+        test_room = Room.objects.create(room_no='1', available=True, capacity=2, room_type='D')
+        Booking.objects.create(bookingid="1", guest=test_user, cin_date="2020-01-01", cout_date="2020-01-03", room_type="D", room=test_room)
 
+    # disse sjekker at bookingen er blitt opprettet og inneholder alle feltene
+    def test_guest(self):
+        booking = Booking.objects.get(bookingid="1")
+        field_label = booking._meta.get_field('guest').verbose_name
+        self.assertEquals(field_label, 'guest')
+
+    def test_cin_date(self):
+        booking = Booking.objects.get(bookingid="1")
+        field_label = booking._meta.get_field('cin_date').verbose_name
+        self.assertEquals(field_label, 'Check-In Date')
+
+    def test_cout_date(self):
+        booking = Booking.objects.get(bookingid="1")
+        field_label = booking._meta.get_field('cout_date').verbose_name
+        self.assertEquals(field_label, 'Check-Out Date')
+
+    def test_room_type(self):
+        booking = Booking.objects.get(bookingid="1")
+        field_label= booking._meta.get_field('room_type').verbose_name
+        self.assertEquals(field_label, 'room type')
+
+    def test_room(self):
+        booking = Booking.objects.get(bookingid="1")
+        field_label= booking._meta.get_field('room').verbose_name
+        self.assertEquals(field_label, 'room')
 
 #tester views
 class ViewTest(TestCase):
@@ -81,4 +114,5 @@ class ViewTest(TestCase):
         #response = self.client.get("/se_rom/")
         #self.assertEqual(response.status_code, 200)
         #self.assertTemplateUsed(response, "../templates/se_rom.html")
+
 
